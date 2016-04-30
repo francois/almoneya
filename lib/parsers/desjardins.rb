88907d747f4d98d8e090d41f0fd87c8b232129e2
François Beausoleil <francois@teksol.info>
2016-04-30 01:34:02 +0000

    Parse Desjardins VISA CSV file format

--- lib/parsers/desjardins.rb
+++ lib/parsers/desjardins.rb
@@ -9,8 +9,14 @@ module Parsers
       bank_accounts = rows.map do |row|
         next if row.map(&:strip).all?(&:empty?)
 
-        account_num = Digest::SHA256.hexdigest(row[0, 3].join(" "))
-        last4       = row[1][-4..-1]
+        account_num = row_to_account_num(row)
+        last4 = if row[1].strip.empty? then
+                  # VISA account
+                  row[0][-4..-1]
+                else
+                  # Checking account
+                  row[1][-4..-1]
+                end
         Repositories::BankAccount.new(nil, account_num, last4)
       end
 
@@ -19,9 +25,15 @@ module Parsers
 
         check_num = row[6].strip.empty? ? nil : row[6].strip
         posted_on = Date.parse(row[3])
-        amount    = row[7].strip.empty? ? BigDecimal(row[8]) : -1 * BigDecimal(row[7])
+        amount = if row[1].strip.empty? then
+                  # VISA account
+                   row[11].strip.empty? ? BigDecimal(row[12]) : -1 * BigDecimal(row[11])
+                 else
+                   # Checking account
+                   row[7].strip.empty? ? BigDecimal(row[8]) : -1 * BigDecimal(row[7])
+                end
 
-        account_num = Digest::SHA256.hexdigest(row[0, 3].join(" "))
+        account_num = row_to_account_num(row)
         Repositories::BankAccountTransaction.new(
           nil,
           account_num,
@@ -35,5 +47,10 @@ module Parsers
 
       [bank_accounts, transactions]
     end
+
+    def row_to_account_num(row)
+      Digest::SHA256.hexdigest(row[0, 3].map(&:strip).reject(&:empty?).join(" "))
+    end
+    private :row_to_account_num
   end
 end
