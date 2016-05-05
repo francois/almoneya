6c52cc09357c4d73b70bd0f1d73a29c639df0ab5
François Beausoleil <francois@teksol.info>
2016-05-05 19:27:59 -0400

    Support all authentication methods from the Scala code

--- src/main/scala/almoneya/SignIn.scala
+++ src/main/scala/almoneya/SignIn.scala
@@ -6,8 +6,25 @@ import org.joda.time.DateTime
 
 sealed trait SignInMethod extends SqlValue
 
+object SignInMethod {
+    def fromString(value: String): Option[SignInMethod] = value match {
+        case "userpass" => Some(UserpassSignIn)
+        case "twitter" => Some(TwitterSignIn)
+        case "facebook" => Some(FacebookSignIn)
+        case _ => None
+    }
+}
+
 case object UserpassSignIn extends SignInMethod {
     override def setParam(statement: PreparedStatement, index: Int): Unit = statement.setString(1 + index, "userpass")
+}
+
+case object TwitterSignIn extends SignInMethod {
+    override def setParam(statement: PreparedStatement, index: Int): Unit = statement.setString(1 + index, "twitter")
+}
+
+case object FacebookSignIn extends SignInMethod {
+    override def setParam(statement: PreparedStatement, index: Int): Unit = statement.setString(1 + index, "facebook")
 }
 
 case class SignIn(id: Option[SignInId] = None,
--- src/main/scala/almoneya/SignInsRepository.scala
+++ src/main/scala/almoneya/SignInsRepository.scala
@@ -12,11 +12,17 @@ import scala.util.Try
 class SignInsRepository(executor: QueryExecutor) {
 
     def create(signIn: SignIn): Try[SignIn] =
-        executor.insertReturning(Query("INSERT INTO credentials.sign_ins(source_ip, user_agent, method, successful) VALUES (?::inet, ?, ?, ?) RETURNING sign_in_id"), signIn.sourceIp, signIn.userAgent, signIn.method, signIn.successful) { rs =>
-            signIn.copy(id = Some(SignInId(rs.getInt("sign_in_id"))))
+        executor.insertReturning(Query("INSERT INTO credentials.sign_ins(source_ip, user_agent, method, successful) VALUES (?::inet, ?, ?, ?) RETURNING sign_in_id, source_ip, user_agent, method, successful, created_at, updated_at"), signIn.sourceIp, signIn.userAgent, signIn.method, signIn.successful) { rs =>
+            signIn.copy(id = Some(SignInId(rs.getInt("sign_in_id"))),
+                sourceIp = IpAddress(rs.getString("source_ip")),
+                userAgent = UserAgent(rs.getString("user_agent")),
+                method = SignInMethod.fromString(rs.getString("method")).get, // the database constraints guarantee that the value is correct
+                successful = rs.getBoolean("successful"),
+                createdAt = new DateTime(rs.getTimestamp("created_at")),
+                updatedAt = new DateTime(rs.getTimestamp("updated_at")))
         }.map(_.head).flatMap { basicSignIn =>
-            executor.insertReturning(Query("INSERT INTO credentials.userpass_sign_ins(sign_in_id, username) VALUES (?, ?) RETURNING *"), basicSignIn.id.get, signIn.username) { rs =>
-                basicSignIn.copy(createdAt = new DateTime(rs.getTimestamp("created_at")), updatedAt = new DateTime(rs.getTimestamp("updated_at")))
+            executor.insertReturning(Query("INSERT INTO credentials.userpass_sign_ins(sign_in_id, username) VALUES (?, ?) RETURNING username"), basicSignIn.id.get, signIn.username) { rs =>
+                basicSignIn.copy(username = Username(rs.getString("username")))
             }.map(_.head)
         }
 }
