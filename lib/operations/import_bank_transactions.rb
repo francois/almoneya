37c9934e29251c2905edc95c40a0a443ccf7998a
François Beausoleil <francois@teksol.info>
2016-05-12 10:30:03 +0000

    Remove TODO that's been done

--- lib/operations/import_bank_transactions.rb
+++ lib/operations/import_bank_transactions.rb
@@ -23,7 +23,6 @@ module Operations
       raise FileTooLarge,  "File #{File.basename(file.path)} exceeds the threshold of #{"%.1f" % [THRESHOLD / 1_000_000]} MB; won't process" if file.size > THRESHOLD
       raise InvalidFormat, "File #{File.basename(file.path)} is empty" if file.size.zero?
 
-      # TODO: We have to determine which bank generated this 
       rows = CSV.read(file.path, encoding: "ISO-8859-1:UTF-8", row_sep: "\r\n", col_sep: ",", headers: false)
       bank_accounts, transactions =
         case rows.last.size
