47c06f89bc836229d4e8e10694b34298a655b507
François Beausoleil <francois@teksol.info>
2016-05-19 21:45:29 -0400

    Prioritize recurring obligations according to priority after date

--- src/main/scala/almoneya/automation/RevenueAllocator.scala
+++ src/main/scala/almoneya/automation/RevenueAllocator.scala
@@ -5,7 +5,7 @@ import org.joda.time.LocalDate
 
 case class RevenueAllocator(obligations: Set[RecurringObligation], goals: Set[FixedDateObligation], revenues: Set[Revenue], autoFulfillThreshold: Amount = Amount(BigDecimal(100))) {
     def generatePlan(paidOn: LocalDate, amountReceived: Amount): Seq[Payment] = {
-        val plan = obligations.toSeq.map(fundingGoal => Payment(fundingGoal, planToTake = fundingGoal.amountMissing)).sortWith((a, b) => a.goal.dueOn.compareTo(b.goal.dueOn) < 0)
+        val plan = obligations.toSeq.map(fundingGoal => Payment(fundingGoal, planToTake = fundingGoal.amountMissing)).sortWith((a, b) => a.goal.dueOn.compareTo(b.goal.dueOn) < 0 || a.goal.priority.compareTo(b.goal.priority) < 0)
         val runningBalances = amountReceived +: plan.indices.tail.map(idx => amountReceived - plan.slice(0, idx).map(_.planToTake).reduce(_ add _))
         plan.zip(runningBalances).map {
             case (payment, balance) if balance.isPositive && balance >= payment.planToTake => payment.copy(realTake = payment.planToTake)
--- test/scala/almoneya/automation/RevenueAllocatorTest.scala
+++ test/scala/almoneya/automation/RevenueAllocatorTest.scala
@@ -53,6 +53,19 @@ class RevenueAllocatorTest extends FunSuite {
         assert(!payments.find(_.goal == alimony).forall(_.fulfilled), "alimoney must be unfulfilled")
     }
 
+    test("prioritizes obligations with lower priority") {
+        val groceries = newWeeklyObligation("groceries", 100, new LocalDate(2016, 5, 22))
+        val alimony = newWeeklyObligation("alimony", 200, new LocalDate(2016, 5, 22), priority = 1)
+        val goals = Set.empty[FixedDateObligation]
+        val obligations = Set(groceries, alimony)
+        val revenues = Set(newWeeklyRevenue("salary", new LocalDate(2016, 5, 20)))
+        val sharer = RevenueAllocator(obligations, goals, revenues)
+        val payments = sharer.generatePlan(new LocalDate(2016, 5, 20), Amount(BigDecimal(201)))
+
+        assert(payments.contains(Payment(alimony, planToTake = Amount(BigDecimal(200)), realTake = Amount(BigDecimal(200)))), "alimony received 99% of the allocation")
+        assert(payments.contains(Payment(groceries, planToTake = Amount(BigDecimal(100)), realTake = Amount(BigDecimal(1)))), "groceries received 1% of the allocation")
+    }
+
     def newWeeklyRevenue(name: String, dueOn: LocalDate): Revenue = {
         Revenue(RevenueName(name), dueOn, Weekly, Frequency(1))
     }
@@ -61,8 +74,8 @@ class RevenueAllocatorTest extends FunSuite {
         FixedDateObligation(priority = Priority(1), name = ObligationName(name), target = Amount(BigDecimal(targetAmount)), balance = Amount(BigDecimal(0)), dueOn = dueOn)
     }
 
-    def newWeeklyObligation(name: String, targetAmount: Int, dueOn: LocalDate): RecurringObligation = {
-        RecurringObligation(priority = Priority(1), name = ObligationName(name), target = Amount(BigDecimal(targetAmount)), balance = Amount(BigDecimal(0)), dueOn = dueOn, period = Weekly, frequency = Frequency(1))
+    def newWeeklyObligation(name: String, targetAmount: Int, dueOn: LocalDate, priority: Int = 100): RecurringObligation = {
+        RecurringObligation(priority = Priority(priority), name = ObligationName(name), target = Amount(BigDecimal(targetAmount)), balance = Amount(BigDecimal(0)), dueOn = dueOn, period = Weekly, frequency = Frequency(1))
     }
 
     def newMonthlyObligation(name: String, targetAmount: Int, dueOn: LocalDate): RecurringObligation = {
