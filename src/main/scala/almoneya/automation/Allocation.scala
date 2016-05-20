ba497e3d9583b5bf4f1efdffd6ae9b030007fd93
François Beausoleil <francois@teksol.info>
2016-05-19 23:13:24 -0400

    Respect the Law of Demeter in Allocation
    This has the benefit of making code easier to read as well.

--- src/main/scala/almoneya/automation/Allocation.scala
+++ src/main/scala/almoneya/automation/Allocation.scala
@@ -1,7 +1,14 @@
 package almoneya.automation
 
 import almoneya.Amount
+import org.joda.time.LocalDate
 
 case class Allocation(goal: FundingGoal, planToTake: Amount, realTake: Amount = Amount(0)) {
     def fulfilled = realTake >= planToTake
+
+    def dueOn: LocalDate = goal.dueOn
+
+    def priority: Priority = goal.priority
+
+    def amountMissing: Amount = goal.amountMissing
 }
